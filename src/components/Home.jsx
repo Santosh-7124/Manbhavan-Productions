934d7687b00b64148b68d90a0c218f2e51a4cc54
Santosh-7124 <sunny962002@gmail.com>
2026-03-10 17:37:09 +0530

    More sections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Contact from "./Home/Contact";
 import Services from "./Home/Services";
 import Banner from "./Home/Banner";
+import Marquee from "./Home/Marquee";
+import Testimony from "./Home/Testimony";
 
 function Home() {
   return (
     <>
       <Banner />
+      <Marquee />
       <Services />
+      <Testimony />
       <Contact />
     </>
   );
---
+++ src/components/Home/Marquee.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Limca from "../../assets/Home/Marquee/Limca.png";
+import Parle from "../../assets/Home/Marquee/Parle.png";
+
+function Marquee() {
+  return (
+    <section className="home-marquee">
+      <div className="home-marquee-wrapper">
+        <div className="home-marquee-container">
+          <img src={Limca} alt="Limca" />
+          <img src={Parle} alt="Parle" />
+          <img src={Limca} alt="Limca" />
+          <img src={Parle} alt="Parle" />
+          <img src={Limca} alt="Limca" />
+          <img src={Parle} alt="Parle" />
+          <img src={Limca} alt="Limca" />
+          <img src={Parle} alt="Parle" />
+          <img src={Limca} alt="Limca" />
+          <img src={Parle} alt="Parle" />
+          <img src={Limca} alt="Limca" />
+          <img src={Parle} alt="Parle" />
+        </div>
+      </div>
+      <div className="home-marquee-left"></div>
+      <div className="home-marquee-right"></div>
+    </section>
+  );
+}
+
+export default Marquee;
